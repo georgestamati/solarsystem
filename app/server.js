17fb33822b6f514d75e8ca3f11eb769f466e5676
George Stamati <gstamati@endava.com>
2017-09-22 15:33:29 +0300

    Remote connection with socket.io

--- app/server.js
+++ app/server.js
@@ -1,13 +1,25 @@
 var app = require('../app')
 var debug = require('debug')('helloworld:server');
 var http = require('http');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // Get port from environment and store in Express.
 var port = process.env.PORT || '3000';
 app.set('port', port);
 
 //  Create HTTP server.
-var server = http.createServer(app);
+// var server = http.createServer(app);
+
+// Socket IO
+io.on('connection', function (socket) {
+    // Create a room to broadcast to
+    socket.join('main');
+    socket.on('statechange', function (data) {
+        // Broadcast changes to all clients in room
+        socket.to('main').emit('urlchange', { url : data.url });
+    });
+});
 
 // Listen on provided port, on all network interfaces.
 server.listen(port);
