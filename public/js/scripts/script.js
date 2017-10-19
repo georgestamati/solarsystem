1c96b3ccdff949ed06f91fa7f106fe5b587c1257
George Stamati <gstamati@endava.com>
2017-10-19 14:22:11 +0300

    Loader done

--- public/js/scripts/script.js
+++ public/js/scripts/script.js
@@ -1,15 +1,42 @@
 $(document).ready(function(){
 
-	if( sessionStorage.getItem('loader') == null ) { 
-		$('.loader').show();
-		sessionStorage.setItem('loader', 'true');
+	var universe = $('#universe');
+	var loader = $('.loader');
 
-		$('.loader__button').on('click', loaderAnimation)
+	if(sessionStorage.getItem('loader')){
+		universe.addClass('hidden')
+		setTimeout(function(){
+			universe.css({
+				'opacity': 1,
+				'visibility': 'visible',
+				'transition': 'all 1s ease'
+			});
+		}, 3000)
+
+		setTimeout(function(){
+			loader.css({
+				'opacity': 0,
+				'visibility': 'hidden',
+				'transition': 'all 1s ease'
+			});
+		}, 3000)
+
+		loader.find('.loader__button').addClass('hidden');
 	}
 	else{
-		$('.loader').hide();
-		loaderAnimation();
+		if( sessionStorage.getItem('loader') == null ) { 
+			loader.show();
+			sessionStorage.setItem('loader', 'true');
+
+			loader.find('.loader__button').on('click', loaderAnimation)
+		}
+		else{
+			loader.hide();
+			loaderAnimation();
+		}		
 	}
+
+
 
 
 	var stars = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
@@ -51,17 +78,15 @@ $(document).ready(function(){
 	else {
 		console.log('Speech Recognition is not supported');
 	}
+	
+	function loaderAnimation(){
+		loader.css({
+			'margin-top': '-1000px',
+			'transition': 'all 1.5s ease-in'
+		}).addClass('hidden');
+		$('#galaxy, .menu').css({
+			'margin-top': 0,
+			'transition': 'all 1.5s ease-in'
+		});
+	}
 })
-
-function loaderAnimation(){
-	$('.loader').css({
-		'opacity': 0,
-		'visibility': 'hidden',
-		'margin-top': '-1000px',
-		'transition': 'all 1.5s ease-in'
-	});
-	$('#galaxy, .menu').css({
-		'margin-top': 0,
-		'transition': 'all 1.5s ease-in'
-	});
-}
