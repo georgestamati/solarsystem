b0f37c1147844bc5f8d6a8b7186226cda97c1a07
George Stamati <gstamati@endava.com>
2017-11-01 15:33:45 +0200

    Show images on planet page from nasa api

--- public/js/scripts/script.js
+++ public/js/scripts/script.js
@@ -35,8 +35,8 @@ $(document).ready(function(){
 		firefoxUserAgent = (/Firefox/i.test(navigator.userAgent));
 
 	// Zoom effect
-	var zoomEvent = firefoxUserAgent ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-	var incr = 1;
+	var zoomEvent = firefoxUserAgent ? "DOMMouseScroll" : "mousewheel", //FF doesn't recognize mousewheel as of FF3.x
+		incr = 1;
 	
 	if (document.attachEvent) //if IE (and Opera depending on user setting)
 		document.attachEvent("on" + zoomEvent, zoomGalaxy)
@@ -50,8 +50,8 @@ $(document).ready(function(){
 
 
 	function zoomGalaxy(e){
-		var ev = window.event || e; //equalize event object
-		var delta = ev.detail ? ev.detail : ev.wheelDelta; //check for detail first so Opera uses that instead of wheelDelta
+		var ev = window.event || e, //equalize event object
+			delta = ev.detail ? ev.detail : ev.wheelDelta; //check for detail first so Opera uses that instead of wheelDelta
 		delta = delta / 120;
 		delta = firefoxUserAgent ? delta / 3 : delta;
 		incr += delta / 5;
@@ -63,8 +63,8 @@ $(document).ready(function(){
 		}
 	}
 	function parallaxIt(e, target, movement){
-		var relX = e.pageX - universe.offset().left;
-		var relY = e.pageY - universe.offset().top;
+		var relX = e.pageX - universe.offset().left,
+			relY = e.pageY - universe.offset().top;
 
 		TweenMax.to(target, 1, {
 			x: (relX - universe.width()/2) / universe.width() * movement,
@@ -73,46 +73,49 @@ $(document).ready(function(){
 	}
 
 
-	var stars = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-	var ua = navigator.userAgent;
-	var socket = io.connect();
+	var stars = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'],
+		ua = navigator.userAgent,
+		socket = io.connect();
 
 	// $('#galaxy').addClass('galaxy-view--3D');
 
-	// $("button").on("click", handleButtonClick);
 
-	// function jsonFlickrFeed(json) {
-	//   console.log(json);
-	  
-	//   $.each(json.items, function(i, item) {
-	//     $("<img />").attr("src", item.media.m).appendTo("#images");
-	//   });
-	// };
 
-	// function handleButtonClick() {
-	//   $("button").remove();
-	  
-	//   $.ajax({
-	//     url: 'https://api.flickr.com/services/feeds/photos_public.gne',
-	//     dataType: 'jsonp',
-	//     data: { "tags": "kitten", "format": "json" }
-	//   });
-	// }
+	// NASA API images
+	var api_key = '98oJMJodoPGnat7vL3xwzjDmN110U4jCwiKVzSJM',
+		query = 'galaxy',
+		media_type = 'image',
+		nasa_url = 'https://images-api.nasa.gov/search?q='+query+'&media_type='+media_type;
+	
+	$.ajax({
+		url: nasa_url,
+		success: function(results){
+			$.each(results.collection.items, function(i, item){
+				if(i >= 10){
+					return false;
+				}
+				$('<img />').attr('src', item.links[0].href).appendTo('.info-gallery')
+			})
+		},
+		error: function(){
+			console.log('error')
+		}
+	})
 
+	//Flickr API images
 	function showFlickrImages(word, no){
-		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text="+ word +"&sort=relevance&privacy_filter=1&safe_search=1&per_page="+ no +"&page=1&format=json&nojsoncallback=1";
+		var flickr_url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text="+ word +"&privacy_filter=1&safe_search=1&per_page="+ no +"&format=json&nojsoncallback=1&content_type=1&media=photos";
 		
 		$.ajax({
-			url: url,
+			url: flickr_url,
 			success: function(res){
 				$.each(res.photos.photo, function(i, item){
-					$('<img src="https://farm'+item.farm+'.staticflickr.com/'+item.server+'/'+item.id+'_'+item.secret+'_n.jpg" />').appendTo('#gallery');  
+					$('<img src="https://farm'+item.farm+'.staticflickr.com/'+item.server+'/'+item.id+'_'+item.secret+'_n.jpg" />').appendTo('.info-gallery');  
 				})
 			}
 		});
 	}
-
-	showFlickrImages('jupiter planet', 10);
+	// showFlickrImages('jupiter planet', 10);
 
 	
 	if (annyang) {
@@ -133,18 +136,25 @@ $(document).ready(function(){
 					}
 				})
             },
-            'images about :tag': function(tag){
-				// var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text="+ word +"&sort=relevance&privacy_filter=1&safe_search=1&per_page="+ no +"&page=1&format=json&nojsoncallback=1";
-				var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6a970fbb976a06193676f88ef2722cc8&text="+ word +"&sort=relevance&privacy_filter=1&safe_search=1&per_page=10&page=1&format=json&nojsoncallback=1";
-		
+            'image about *search': function(search){
+				var api_key = '98oJMJodoPGnat7vL3xwzjDmN110U4jCwiKVzSJM',
+					media_type = 'image',
+					nasa_url = 'https://images-api.nasa.gov/search?q='+search+'&title='+search+'&media_type='+media_type+'&year_start=1900';
+				
 				$.ajax({
-					url: url,
-					success: function(res){
-						$.each(res.photos.photo, function(i, item){
-							$('<img src="https://farm'+item.farm+'.staticflickr.com/'+item.server+'/'+item.id+'_'+item.secret+'_n.jpg" />').appendTo('#gallery');  
+					url: nasa_url,
+					success: function(results){
+						$.each(results.collection.items, function(i, item){
+							if(i >= 10){
+								return false;
+							}
+							$('<img />').attr('src', item.links[0].href).appendTo('.info-gallery')
 						})
+					},
+					error: function(){
+						console.log('error')
 					}
-				});
+				})
 			}
         };
 
