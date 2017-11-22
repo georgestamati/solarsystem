050def935b969fd308a15a48eb944b0677b533ce
George Stamati <george.stamati@yahoo.com>
2017-11-22 18:09:39 +0200

    orbit inclination animation + db info added + tooltip with info

--- app/db.js
+++ app/db.js
@@ -1,134 +1,330 @@
 var rows = [
 	{	
-		name: 'sun',
-		moons: [
+		'name': 'sun',
+        'description': {
+		    'diameter': '1,392,530 km',
+            'rotation period': '25-35 days',
+            'orbit period': '',
+            'axis inclination': '0.00°',
+            'orbit inclination': ''
+        },
+		'moons': [
             {
-                name: 'mercury'
+                'name': 'mercury',
+                'description': {
+                    'diameter': '4,879.4 km',
+                    'rotation period': '58.65 days',
+                    'orbit period': '0.241 year / 87.97 days',
+                    'axis inclination': '0.00°',
+                    'orbit inclination': '7.004°'
+                }
             },
             {
-                name: 'venus'
+                'name': 'venus',
+                'description': {
+                    'diameter': '12,103.6 km',
+                    'rotation period': '243.02 days',
+                    'orbit period': '224.7 days',
+                    'axis inclination': '3°',
+                    'orbit inclination': '3.394°'
+                }
             },
             {
-                name: 'earth'
+                'name': 'earth',
+                'description': {
+                    'diameter': '12,756 km',
+                    'rotation period': '0.9972 day',
+                    'orbit period': '365.25 days',
+                    'axis inclination': '23.45°',
+                    'orbit inclination': '0°'
+                }
             },
             {
-                name: 'mars'
+                'name': 'mars',
+                'description': {
+                    'diameter': '6,794 km',
+                    'rotation period': '1.026 days',
+                    'orbit period': '686.98 days',
+                    'axis inclination': '23.98°',
+                    'orbit inclination': '1.85°'
+                }
             },
             {
-                name: 'jupiter'
+                'name': 'jupiter',
+                'description': {
+                    'diameter': '139,822 km',
+                    'rotation period': '9.84 hours',
+                    'orbit period': '11.8623 years',
+                    'axis inclination': '3.12°',
+                    'orbit inclination': '1.31°'
+                }
             },
             {
-                name: 'saturn',
-				rings: '.rings'
+                'name': 'saturn',
+                'description': {
+                    'diameter': '120,536 km',
+                    'rotation period': '10.23 days',
+                    'orbit period': '29.458 years',
+                    'axis inclination': '26.73°',
+                    'orbit inclination': '2.49°'
+                },
+				'rings': '.rings'
             },
             {
-                name: 'uranus',
-				rings: '.rings'
+                'name': 'uranus',
+                'description': {
+                    'diameter': '51,118 km',
+                    'rotation period': '17.3 hours',
+                    'orbit period': '84.01 years',
+                    'axis inclination': '97.86°',
+                    'orbit inclination': '0.774°'
+                },
+				'rings': '.rings'
             },
             {
-                name: 'neptune'
+                'name': 'neptune',
+                'description': {
+                    'diameter': '49,492 km',
+                    'rotation period': '15.8 hours',
+                    'orbit period': '165 years',
+                    'axis inclination': '29.31°',
+                    'orbit inclination': '1.774°'
+                }
             }
 		]
 	},
 	{	
-		name: 'mercury'
+		'name': 'mercury'
 	},
 	{	
-		name: 'venus'
+		'name': 'venus'
 	},
 	{	
-		name: 'earth',
-		moons: [
+		'name': 'earth',
+		'moons': [
 			{			
-				name: 'moon'
+				'name': 'moon',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	},
 	{	
-		name: 'mars',
-		moons: [
+		'name': 'mars',
+		'moons': [
 			{			
-				name: 'phobos'
+				'name': 'phobos',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'deimos'
+				'name': 'deimos',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	},
 	{	
-		name: 'jupiter',
-		moons: [
+		'name': 'jupiter',
+		'moons': [
 			{			
-				name: 'callisto'
+				'name': 'callisto',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'europa'
+				'name': 'europa',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'ganymede'
+				'name': 'ganymede',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'io'
+				'name': 'io',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	},
 	{	
-		name: 'saturn',
-		moons: [
+		'name': 'saturn',
+		'moons': [
 			{			
-				name: 'dione'
+				'name': 'dione',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'enceladus'
+				'name': 'enceladus',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'iapetus'
+				'name': 'iapetus',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'mimas'
+				'name': 'mimas',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'rhea'
+				'name': 'rhea',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'tethys'
+				'name': 'tethys',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'titan'
+				'name': 'titan',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	},
 	{	
-		name: 'uranus',
-		moons: [
+		'name': 'uranus',
+		'moons': [
 			{			
-				name: 'ariel'
+				'name': 'ariel',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'miranda'
+				'name': 'miranda',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'oberon'
+				'name': 'oberon',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'titania'
+				'name': 'titania',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'umbriel'
+				'name': 'umbriel',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	},
 	{	
-		name: 'neptune',
-		moons: [
+		'name': 'neptune',
+		'moons': [
 			{			
-				name: 'proteus'
+				'name': 'proteus',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			},
 			{			
-				name: 'triton'
+				'name': 'triton',
+                'description': {
+				    'diameter': '',
+                    'rotation period': '',
+                    'axis inclination': '',
+                    'orbit inclination': ''
+                }
 			}
 		]
 	}
 ];
+
+
+for (var row in rows){
+    if (row > 0) {
+        rows[row].description = rows[0].moons[row - 1].description;
+    }
+}
 
 module.exports = rows;
